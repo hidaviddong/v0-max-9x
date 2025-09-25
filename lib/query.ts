46cb1691027068d0644b035af6977e44b25bc9f4
David Dong <hidaviddong@gmail.com>
2025-09-25 10:49:57 +0800

    feat: pass the clerk token

--- lib/query.ts
+++ lib/query.ts
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { type Asset } from "@/components/data-table";
 import { parseAsString, useQueryState } from "nuqs";
 import { useDebouncedValue } from "foxact/use-debounced-value";
+import { useSession } from "@clerk/nextjs";
 
 export const KEYS = {
   ASSETS: "assets",
@@ -25,30 +26,28 @@ function transformApiData(apiData: any): Asset[] {
 
 export function useAssets() {
   const [searchQuery] = useQueryState("search", parseAsString);
-  const searchDebouncedValue = useDebouncedValue(
-    searchQuery,
-    // delay in ms
-    300,
-    // optional, default to false. whether to immediately update the debounced value with the first call
-    false
-  );
-
-  console.log("searchDebouncedValue", searchDebouncedValue);
+  const searchDebouncedValue = useDebouncedValue(searchQuery, 300, false);
+  const { session } = useSession();
   const assetsQuery = useQuery({
-    queryKey: [KEYS.ASSETS, searchDebouncedValue || "all"],
+    queryKey: [KEYS.ASSETS, searchDebouncedValue || "all", session?.id],
     queryFn: async () => {
       const url = searchDebouncedValue
         ? `${BASE_URL}/search_assets?q=${searchDebouncedValue}`
         : `${BASE_URL}/all_assets`;
 
-      const response = await fetch(url);
+      const token = await session?.getToken();
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       const data = await response.json();
       return data;
     },
     select(data) {
       return transformApiData(data);
     },
+    enabled: !!session,
   });
-
   return assetsQuery;
 }
