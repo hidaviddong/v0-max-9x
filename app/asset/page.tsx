45ba34f7eae40515429b12130dee8f2a317161b2
David Dong <hidaviddong@gmail.com>
2025-09-25 17:28:49 +0800

    feat: signle file

--- app/asset/page.tsx
+++ app/asset/page.tsx
@@ -1,18 +1,62 @@
 "use client";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
-import { createColumns, DataTable } from "@/components/data-table";
+import { type Asset, createColumns, DataTable } from "@/components/data-table";
 import { useLanguage } from "@/contexts/language-context";
-import { useAssets } from "@/lib/query";
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useDebouncedValue } from "foxact/use-debounced-value";
+import { useSession } from "@clerk/nextjs";
+import { toast } from "sonner";
+
+function transformApiData(apiData: any): Asset[] {
+  return apiData.map((item: any) => ({
+    "Asset ID": item._source["Asset ID"],
+    "Asset Name": item._source["Asset Name"],
+    Link: item._source.Link,
+    Impact: item._source.Impact,
+    Heat: item._source.Heat,
+    Profitability: item._source.Profitability,
+    Portfolio: item._source.Portfolio,
+    "Listing Date": item._source["Listing Date"],
+    Type: item._source.Type,
+  }));
+}
 
 export default function AssetPage() {
   const { t } = useLanguage();
+
   const [search, setSearch] = useState("");
-  const assetsQuery = useAssets(search);
-  const isLoading = assetsQuery.isLoading;
-  const data = assetsQuery.data || [];
+  const debouncedSearch = useDebouncedValue(search, 300, false);
+  const { session } = useSession();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["assets", debouncedSearch || "all", session?.id],
+    queryFn: async () => {
+      const url = debouncedSearch
+        ? `${
+            process.env.NEXT_PUBLIC_FASTAPI_URL
+          }/search_assets?q=${encodeURIComponent(String(debouncedSearch))}`
+        : `${process.env.NEXT_PUBLIC_FASTAPI_URL}/all_assets`;
+      const token = await session?.getToken();
+      const response = await fetch(url, {
+        headers: token
+          ? {
+              Authorization: `Bearer ${token}`,
+            }
+          : undefined,
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        toast.error(data?.detail || "Request failed");
+      }
+      return data;
+    },
+    select: (data) => transformApiData(data),
+    enabled: !!session,
+  });
   const columns = createColumns(t);
+
   return (
     <div className="min-h-screen bg-black text-white">
       <Header />
@@ -30,7 +74,7 @@ export default function AssetPage() {
           <DataTable
             isLoading={isLoading}
             columns={columns}
-            data={data}
+            data={data || []}
             searchValue={search}
             onSearchChange={setSearch}
           />
