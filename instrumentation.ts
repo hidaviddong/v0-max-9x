3b102a2ef7bef4e89f537389813c0234799ab125
David Dong <hidaviddong@gmail.com>
2025-09-26 18:25:27 +0800

    feat: add signoz

---
+++ instrumentation.ts
@@ -0,0 +1,15 @@
+import { registerOTel, OTLPHttpJsonTraceExporter } from "@vercel/otel";
+// Add otel logging
+import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ERROR); // set diaglog level to DEBUG when debugging
+export function register() {
+  registerOTel({
+    serviceName: "<service_name>",
+    traceExporter: new OTLPHttpJsonTraceExporter({
+      url: "https://ingest.us.signoz.cloud:443/v1/traces",
+      headers: {
+        "signoz-ingestion-key": process.env.SIGNOZ_INGESTION_KEY,
+      },
+    }),
+  });
+}
